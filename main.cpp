4ee48e3a791ac064661cbe3d1a6999c67cc9f68e
Juro12 <54584470+Juro12@users.noreply.github.com>
2019-08-27 15:00:05 +0200

    my first respository

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int NWD(int a, int b)
+{
+	int p;
+
+  	while(b!=0)
+	{
+    	p = b;
+    	b = a%b;
+    	a = p;
+  	}
+    return a;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int tab[n];
+    int A,B;
+
+    for(int i = 0; i < n; i++){
+        cin >> A >> B;
+        tab[i] = NWD(A,B);
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << tab[i] << endl;
+    }
+
+    return 0;
+}
